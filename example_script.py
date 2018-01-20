c89a64a655faef23ba764adc27c92b0cb898c532
Jamie Bernardi <jamie.bernardii@gmail.com>
2018-01-20 18:55:54 +0000

    auto upload

--- example_script.py
+++ example_script.py
@@ -51,16 +51,16 @@ def get_welcome_response():
     session_attributes = {}
     card_title = "Welcome"
     speech_output = "Welcome to the My Spelling Bee Alexa Skill. " \
-                    "Tell me what to do, or ask me to list your options"\
+                    "Tell me your facourite colour"\
                     
     # If the user either does not reply to the welcome message or says something
     # that is not understood, they will be prompted again with this text.
-    reprompt_text = "Do you want me to list your options?"
-    
+    reprompt_text = "Please tell me your favorite color by saying, " \
+                    "my favorite color is red."
     should_end_session = False
-    
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
+
 
 def handle_session_end_request():
     card_title = "Session Ended"
@@ -71,32 +71,9 @@ def handle_session_end_request():
     return build_response({}, build_speechlet_response(
         card_title, speech_output, None, should_end_session))
 
+
 def create_favorite_color_attributes(favorite_color):
-    
     return {"favoriteColor": favorite_color}
-
-def list_options():
-    cart_title = intent['list']
-    session_attributes = {}
-    should_end_session = False
-
-        "Select from the following options: " \
-                    "Test on some sample words" \
-                    "Add a word to a dictionary" \
-                    "List your dictionaries" \
-                    "Create a dictionary" \
-                    "Create a new dictionary from a webpage" \
-                    "Merge two diction"
-
-def spelling_test(intent, session): 
-     
-     card_title = intent['name']
-     session_attributes = {}
-     should_end_session = False
-
-     
-
-
 
 def set_color_in_session(intent, session):
     """ Sets the color in the session and prepares the speech to reply to the
@@ -125,6 +102,7 @@ def set_color_in_session(intent, session):
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
 
+
 def get_color_from_session(intent, session):
     session_attributes = {}
     reprompt_text = None
@@ -146,7 +124,7 @@ def get_color_from_session(intent, session):
         intent['name'], speech_output, reprompt_text, should_end_session))
 
 
-# --------------- Events ------------------ #
+# --------------- Events ------------------
 
 def on_session_started(session_started_request, session):
     """ Called when the session starts """
@@ -176,18 +154,14 @@ def on_intent(intent_request, session):
     intent_name = intent_request['intent']['name']
 
     # Dispatch to your skill's intent handlers
-    if intent_name == "SpellingTest":
-        return spelling_test(intent, session)
-
+    if intent_name == "MyColorIsIntent":
+        return set_color_in_session(intent, session)
     elif intent_name == "WhatsMyColorIntent":
         return get_color_from_session(intent, session)
-    
     elif intent_name == "AMAZON.HelpIntent":
         return get_welcome_response()
-    
     elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
         return handle_session_end_request()
-    
     else:
         raise ValueError("Invalid intent")
 
